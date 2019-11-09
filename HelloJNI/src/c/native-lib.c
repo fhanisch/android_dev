026417cd0ff22fd34bfbb589d65d315ad1decc2d
fhanisch <felix.hanisch@live.de>
2019-11-09 20:41:02 +0100

    set targetSdkVersion becaus notify of any app permissions at install time

--- HelloJNI/src/c/native-lib.c
+++ HelloJNI/src/c/native-lib.c
@@ -6,10 +6,10 @@ JNIEXPORT jstring JNICALL Java_com_hellojni_HelloJNI_stringFromJNI(JNIEnv* env, 
 {
 	char msg[] = "HalliHallo JNI!!!\n";
 	char str[] = "Das ist das Haus vom Nikolaus.\n";
-	//FILE* file = fopen("/storage/emulated/0/Dokumente/HelloJNI.log", "a");
-	//fwrite(msg, strlen(msg), 1, file);
-	//fwrite(str, strlen(str), 1, file);
-	//fclose(file);
+	FILE* file = fopen("/storage/emulated/0/Dokumente/HelloJNI.log.txt", "a");
+	fwrite(msg, strlen(msg), 1, file);
+	fwrite(str, strlen(str), 1, file);
+	fclose(file);
 
 	return (*env)->NewStringUTF(env, str);
 }
