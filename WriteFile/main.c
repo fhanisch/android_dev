1ef809ab77904e616bbec7d2f04021fa440300c1
fhanisch <felix.hanisch@googlemail.com>
2019-11-30 01:14:43 +0100

    internal data path

--- WriteFile/main.c
+++ WriteFile/main.c
@@ -4,16 +4,23 @@
 
 /**
 * Dies ist der Haupteinsprungspunkt einer systemeigenen Anwendung, die
-* android_native_app_glue verwendet. Sie wird im eigenen Thread ausgeführt, mit ihrer eigenen
-* Ereignisschleife zum Empfangen von Eingabeereignissen und Ausführen anderer Dinge.
+* android_native_app_glue verwendet. Sie wird im eigenen Thread ausgefï¿½hrt, mit ihrer eigenen
+* Ereignisschleife zum Empfangen von Eingabeereignissen und Ausfï¿½hren anderer Dinge.
 */
 void android_main(struct android_app* state)
 {
 	char msg[] = "HalliHallo!!!\n";
 	char str[] = "Das ist das Haus vom Nikolaus.\n";
+	const char* internalDataPath;
+
+	internalDataPath = state->activity->internalDataPath;
+
 	FILE* file = fopen("/storage/emulated/0/Dokumente/log.txt", "a");
+	if (file == NULL) return;
+
 	fwrite(msg, strlen(msg), 1, file);
 	fwrite(str, strlen(str), 1, file);
+	fwrite(internalDataPath, strlen(internalDataPath), 1, file);
 	fclose(file);
 	while (1);
 }
